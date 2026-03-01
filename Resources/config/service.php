da6ba569a4384b11c8e039c42d6a8f7d4f568224
MichelAngelo <baksdevelopmen@gmail.com>
2026-03-02 01:41:19 +0300

    BaksDevelopment

--- Resources/config/service.php
+++ Resources/config/service.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -30,8 +30,9 @@ return static function (ContainerConfigurator $configurator) {
 
     $services = $configurator->services()
         ->defaults()
-        ->autowire(true)
-        ->autoconfigure(true);
+        ->autowire()
+        ->autoconfigure()
+        ->public();
 
     $NAMESPACE = BaksDevFieldCountryBundle::NAMESPACE;
     $PATH = BaksDevFieldCountryBundle::PATH;
